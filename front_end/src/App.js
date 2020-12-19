bee62d546a5c731c0b787a8dcd8a2942dab1ab17
Rafael <rsantiago@graviton-tech.com>
2020-12-18 19:37:03 -0600

    se agrego redux y middleware para el front

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,183 +1,39 @@
-import React, { useState, useEffect, useRef } from 'react';
-import classNames from 'classnames';
-import { Route } from 'react-router-dom';
-import { CSSTransition } from 'react-transition-group';
+import React from 'react';
 
-import { AppTopbar } from './AppTopbar';
-import { AppFooter } from './AppFooter';
-import { AppMenu } from './AppMenu';
-import { AppProfile } from './AppProfile';
-import { AppConfig } from './AppConfig';
-
+import { Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./redux/store";
+import { history } from './jwt/_helpers';
+import { PrivateRoute } from './routes/PrivateRoutes';
 import Login from './pages/Login'
-import {Usuarios} from './components/Usuarios';
+import AppMain from './AppMain';
 
-import PrimeReact from 'primereact/utils';
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
-import 'prismjs/themes/prism-coy.css';
-import '@fullcalendar/core/main.css';
-import '@fullcalendar/daygrid/main.css';
-import '@fullcalendar/timegrid/main.css';
-import './layout/flags/flags.css';
-import './layout/layout.scss';
-import './App.scss';
-
-const App = () => {
-
-    const [layoutMode, setLayoutMode] = useState('static');
-    const [layoutColorMode, setLayoutColorMode] = useState('dark')
-    const [staticMenuInactive, setStaticMenuInactive] = useState(false);
-    const [overlayMenuActive, setOverlayMenuActive] = useState(false);
-    const [mobileMenuActive, setMobileMenuActive] = useState(false);
-    const [inputStyle, setInputStyle] = useState('outlined');
-    const [ripple, setRipple] = useState(false);
-    const sidebar = useRef();
-    let menuClick = false;
-
-    useEffect(() => {
-        if (mobileMenuActive) {
-            addClass(document.body, 'body-overflow-hidden');
-        }
-        else {
-            removeClass(document.body, 'body-overflow-hidden');
-        }
-    }, [mobileMenuActive]);
-
-    const onInputStyleChange = (inputStyle) => {
-        setInputStyle(inputStyle);
+        this.state = {
+            currentUser: null
+        };
     }
 
-    const onRipple = (e) => {
-        PrimeReact.ripple = e.value;
-        setRipple(e.value)
+
+    render() {
+
+        return (
+
+            <Provider store={configureStore()}>
+                <Router basename="/" history={history}>
+                    <Switch>
+
+                        <PrivateRoute path="/" exact component={AppMain} />
+                        <Route path="/authentication/login" exact component={Login} />
+
+                    </Switch>
+                </Router>
+            </Provider>
+        )
     }
-
-    const onLayoutModeChange = (mode) => {
-        setLayoutMode(mode)
-    }
-
-    const onColorModeChange = (mode) => {
-        setLayoutColorMode(mode)
-    }
-
-    const onWrapperClick = (event) => {
-        if (!menuClick) {
-            setOverlayMenuActive(false);
-            setMobileMenuActive(false);
-        }
-        menuClick = false;
-    }
-
-    const onToggleMenu = (event) => {
-        menuClick = true;
-
-        if (isDesktop()) {
-            if (layoutMode === 'overlay') {
-                setOverlayMenuActive(prevState => !prevState);
-            }
-            else if (layoutMode === 'static') {
-                setStaticMenuInactive(prevState => !prevState);
-            }
-        }
-        else {
-            setMobileMenuActive(prevState => !prevState);
-        }
-        event.preventDefault();
-    }
-
-    const onSidebarClick = () => {
-        menuClick = true;
-    }
-
-    const onMenuItemClick = (event) => {
-        if (!event.item.items) {
-            setOverlayMenuActive(false);
-            setMobileMenuActive(false);
-        }
-    }
-
-    const menu = [
-         { label: 'Usuarios', icon: 'pi pi-fw pi-users', to: '/' }
-       ];
-    const addClass = (element, className) => {
-        if (element.classList)
-            element.classList.add(className);
-        else
-            element.className += ' ' + className;
-    }
-
-    const removeClass = (element, className) => {
-        if (element.classList)
-            element.classList.remove(className);
-        else
-            element.className = element.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-    }
-
-    const isDesktop = () => {
-        return window.innerWidth > 1024;
-    }
-
-    const isSidebarVisible = () => {
-        if (isDesktop()) {
-            if (layoutMode === 'static')
-                return !staticMenuInactive;
-            else if (layoutMode === 'overlay')
-                return overlayMenuActive;
-            else
-                return true;
-        }
-
-        return true;
-    }
-
-    const logo = layoutColorMode === 'dark' ? 'assets/layout/images/logo-white.svg' : 'assets/layout/images/logo.svg';
-
-    const wrapperClass = classNames('layout-wrapper', {
-        'layout-overlay': layoutMode === 'overlay',
-        'layout-static': layoutMode === 'static',
-        'layout-static-sidebar-inactive': staticMenuInactive && layoutMode === 'static',
-        'layout-overlay-sidebar-active': overlayMenuActive && layoutMode === 'overlay',
-        'layout-mobile-sidebar-active': mobileMenuActive,
-        'p-input-filled': inputStyle === 'filled',
-        'p-ripple-disabled': ripple === false
-    });
-
-    const sidebarClassName = classNames('layout-sidebar', {
-        'layout-sidebar-dark': layoutColorMode === 'dark',
-        'layout-sidebar-light': layoutColorMode === 'light'
-    });
-
-    return (
-        <div className={wrapperClass} onClick={onWrapperClick}>
-            <AppTopbar onToggleMenu={onToggleMenu} />
-
-            <CSSTransition classNames="layout-sidebar" timeout={{ enter: 200, exit: 200 }} in={isSidebarVisible()} unmountOnExit>
-                <div ref={sidebar} className={sidebarClassName} onClick={onSidebarClick}>
-                    <div className="layout-logo">
-                        <img alt="Logo" src={logo} />
-                    </div>
-                    <AppProfile />
-                    <AppMenu model={menu} onMenuItemClick={onMenuItemClick} />
-                </div>
-            </CSSTransition>
-
-          
-
-            <div className="layout-main">
-                <Route path="/" exact component={Usuarios} />
-                <Route path="/login" exact component={Login} />
-
-            </div>
-
-            <AppFooter />
-
-        </div>
-    );
-
 }
 
 export default App;
